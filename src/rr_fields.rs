5d9e82969c2bcdacea22c208179a4e6c68037330
Ronald Eytchison <138417975+toponym@users.noreply.github.com>
2023-09-06 23:22:11 -0400

    Build DNS Queries

---
+++ src/rr_fields.rs
@@ -0,0 +1,10 @@
+/// Enums with values for DNS Resource Record (RR) fields
+
+pub enum Type {
+    A = 1,
+}
+// allow non-camel case to match the DNS name for these values
+#[allow(non_camel_case_types)]
+pub enum Class {
+    CLASS_IN = 1, //CLASS_IN
+}
