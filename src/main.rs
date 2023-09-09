adc363707836ab4fec8ad5274d3f957c44642b02
Ronald Eytchison <138417975+toponym@users.noreply.github.com>
2023-09-09 12:51:19 -0400

    Send DNS query to server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use dnsvisor::{self, rr_fields::Type};
+
 fn main() {
-    println!("Hello, world!");
+    let query = dnsvisor::query::build_query(String::from("www.example.com"), Type::A);
+    dnsvisor::socket::query_google(&query);
 }
