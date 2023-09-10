6aa5bf367bd0f8febcfe06cf3c258aa5c52b4b57
Ronald Eytchison <138417975+toponym@users.noreply.github.com>
2023-09-09 21:49:46 -0400

    Fix bug in name decoding and add unit tests

--- src/header.rs
+++ src/header.rs
@@ -2,7 +2,7 @@ use bincode::Options;
 use serde::{Deserialize, Serialize};
 use std::io::Cursor;
 use std::io::Read;
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct DnsHeader {
     pub id: u16,
     pub flags: u16,
@@ -27,5 +27,31 @@ impl DnsHeader {
             .with_big_endian()
             .with_fixint_encoding();
         options.deserialize(&buf).unwrap()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use hex;
+    use pretty_assertions::assert_eq;
+    use std::io::Cursor;
+    #[test]
+    fn header_sample_response() {
+        let response = "60568180000100010000000003777777076578616d706c6503636f6d0000010001c00c000100010000529b00045db8d822";
+        let response_bytes = hex::decode(response).unwrap();
+        let mut reader = Cursor::new(response_bytes.as_slice());
+        let header = DnsHeader::from_bytes(&mut reader);
+        let expected = DnsHeader {
+            id: 24662,
+            flags: 33152,
+            num_questions: 1,
+            num_answers: 1,
+            num_authorities: 0,
+            num_additionals: 0,
+        };
+        let expected_end_pos = 12;
+        assert_eq!(header, expected);
+        assert_eq!(expected_end_pos, reader.position());
     }
 }
