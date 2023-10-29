e891cc1ac28f7c170c7a0ad13bd4deb70d8ec95c
Ronald Eytchison <138417975+toponym@users.noreply.github.com>
2023-10-29 12:12:19 -0400

    Add logging

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use rr_fields::Type;
+use log::{debug, info};
 // TODO visibility
 pub mod header;
 pub mod packet;
@@ -13,13 +14,16 @@ pub fn resolve(domain_name: &str, record_type: Type) -> String {
     let root_nameserver = String::from("198.41.0.4");
     let mut nameserver = root_nameserver;
     loop {
-        println!("[+] Querying {} for {}", nameserver, domain_name);
+        info!("Querying {} for {}", nameserver, domain_name);
         let response = query::send_query(&nameserver, domain_name, record_type);
         if let Some(ip) = response.get_answer(){
+            debug!("Got ip: {}", ip);
             return ip;
         } else if let Some(ns_ip) = response.get_nameserver_ip(){
+            debug!("Got nameserver ip: {}", ns_ip);
             nameserver = ns_ip;
         } else if let Some(ns_domain) = response.get_nameserver(){
+            debug!("Got nameserver domain: {}", ns_domain);
             nameserver = resolve(&ns_domain, Type::A);
         }
     }
