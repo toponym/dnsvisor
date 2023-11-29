f869983e83e5074ed2a70890ca44c03fd6d70d69
Ronald Eytchison <138417975+toponym@users.noreply.github.com>
2023-11-28 21:20:31 -0500

    Support CNAME records

--- src/lib.rs
+++ src/lib.rs
@@ -9,16 +9,26 @@ pub mod record;
 pub mod rr_fields;
 pub mod util;
 
-pub fn resolve(domain_name: &str, record_type: Type) -> String {
+pub fn resolve(req_domain_name: &str, record_type: Type) -> String {
     // Verisign root nameserver
     let root_nameserver = String::from("198.41.0.4");
     let mut nameserver = root_nameserver;
+    let mut domain_name = String::from(req_domain_name);
     loop {
         info!("Querying {} for {}", nameserver, domain_name);
-        let response = query::send_query(&nameserver, domain_name, record_type);
-        if let Some(ip) = response.get_answer() {
-            debug!("Got ip: {}", ip);
-            return ip.to_string();
+        let response = query::send_query(&nameserver, &domain_name, record_type);
+        if let Some((answer_type, answer)) = response.get_answer() {
+            match answer_type {
+                Type::A => {
+                    debug!("Got ip: {}", answer);
+                    return answer.to_string();
+                }
+                Type::CNAME => {
+                    debug!("Got CNAME domain: {}", answer);
+                    domain_name = answer.to_string();
+                }
+                _ => panic!("Unexpected answer type: {:?}", answer_type),
+            }
         } else if let Some(ns_ip) = response.get_nameserver_ip() {
             debug!("Got nameserver ip: {}", ns_ip);
             nameserver = ns_ip.to_string();
